1cfc243005ffa422c42a06f7a68638accb3e72ce
NoahHamilton1 <56700333+NoahHamilton1@users.noreply.github.com>
2025-02-15 19:13:28 +0000

    Improved UI design to change the look and feel of each layer in it's different state. Added an alert to confirm when user wants to delete a layer.

--- pixelPaint/app/src/main/java/org/scc200g15/gui/layerselector/LayerMenuItem.java
+++ pixelPaint/app/src/main/java/org/scc200g15/gui/layerselector/LayerMenuItem.java
@@ -1,12 +1,15 @@
 package org.scc200g15.gui.layerselector;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
@@ -15,10 +18,20 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 
 public class LayerMenuItem extends JPanel {
-    private static String visibleIconPath = "/Icons/open_eye_icon.png";
-    private static String invisibleIconPath = "/Icons/closed_eye_icon.png";
+    private static String eyeOpenIconPath = "/Icons/open_eye_icon.png";
+    private static String eyeShutIconPath = "/Icons/closed_eye_icon.png";
     private static String trashIconPath = "/Icons/trash_icon.png";
-  
+
+    // Colours for the layer menu background as seen in the UI design
+    private final Color VISIBLE_BACKGROUND_COLOUR = new Color(227,227,227);
+    private final Color HIDDEN_BACKGROUND_COLOUR = new Color(233,233,233);
+    private final Color ACTIVE_BACKGROUND_COLOUR = new Color(64,64,64);
+    
+    // Colours for the icons/text as seen in the UI design
+    private final Color VISIBLE_ICON_COLOUR = new Color(128,128,128);
+    private final Color HIDDEN_ICON_COLOUR = new Color(191,191,191);
+    private final Color ACTIVE_ICON_COLOUR = new Color(227,227,227);
+
     public ImageIcon createImageIcon(int x, int y, String path) {
       return new ImageIcon(
         new ImageIcon(getClass().getResource(path))
@@ -26,51 +39,124 @@ public class LayerMenuItem extends JPanel {
           .getScaledInstance(x, y, Image.SCALE_SMOOTH)
       );
     }
-  
-    private ImageIcon visibleIcon = createImageIcon(30, 30, visibleIconPath);
-    private ImageIcon invisibleIcon = createImageIcon(30, 30, invisibleIconPath);
-    private ImageIcon trashIcon = createImageIcon(25, 25, trashIconPath);
 
-    private JButton displayButton = new JButton(this.visibleIcon);
-    private JButton removeButton = new JButton(this.trashIcon);
-    private JLabel layerLabel;
+    public ImageIcon createImageIcon(int x, int y, Color color, String path) {
+        return changeColour(createImageIcon(x, y, path), color);
+    }
+
+    public BufferedImage convert_ImageIcon_To_BufferedImage(ImageIcon icon) {
+        // Code from: Werner Kvalem Vesterås (https://stackoverflow.com/questions/15053214/converting-an-imageicon-to-a-bufferedimage)
+        BufferedImage image = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics g = image.createGraphics();
+        icon.paintIcon(null, g, 0, 0);
+        g.dispose();
+        return image;
+    }
+
+    public ImageIcon changeColour(ImageIcon icon, Color newColor) {
+        BufferedImage image = convert_ImageIcon_To_BufferedImage(icon);
+
+        // Code from: foowtf (https://stackoverflow.com/questions/8029903/how-to-change-the-color-of-an-icon-based-on-user-action)
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                Color imageColor = new Color(image.getRGB(x, y), true);
+                // New Colours but maintain the originally transparent pixels.
+                Color newPixelColor = new Color(newColor.getRed(), newColor.getGreen(), newColor.getBlue(), imageColor.getAlpha());
+                image.setRGB(x, y, newPixelColor.getRGB());
+            }
+        }
+
+        return new ImageIcon(image);
+    }
+
+    // The "eye open" icon is only used in visible/active states
+    private final ImageIcon VISIBLE_EYE_OPEN_ICON = createImageIcon(30, 30, VISIBLE_ICON_COLOUR, eyeOpenIconPath);
+    private final ImageIcon ACTIVE_VISIBLE_ICON = createImageIcon(30, 30, ACTIVE_ICON_COLOUR, eyeOpenIconPath);
+
+    // The "eye shut" icon is only used in hidden/active states
+    private final ImageIcon HIDDEN_EYE_SHUT_ICON = createImageIcon(30, 30, HIDDEN_ICON_COLOUR, eyeShutIconPath);
+    private final ImageIcon ACTIVE_INVISIBLE_ICON = createImageIcon(30, 30, ACTIVE_ICON_COLOUR, eyeShutIconPath);
+
+    // The "trash" icon is used in all states
+    private final ImageIcon VISIBLE_TRASH_ICON = createImageIcon(25, 25, VISIBLE_ICON_COLOUR, trashIconPath);
+    private final ImageIcon HIDDEN_TRASH_ICON = createImageIcon(25, 25, HIDDEN_ICON_COLOUR, trashIconPath);
+    private final ImageIcon ACTIVE_TRASH_ICON = createImageIcon(25, 25, ACTIVE_ICON_COLOUR, trashIconPath);
+    
+    // Actual Components of a LayerMenuItem
+    private JButton displayButton = new JButton(VISIBLE_EYE_OPEN_ICON);
+    private JButton removeButton = new JButton(VISIBLE_TRASH_ICON);
+    private JLabel layerLabel = new JLabel();
+
+    private boolean isVisible = true;
   
     public LayerMenuItem(String layerName, PLayerSelector Manager) {
-        setLayout(new BorderLayout());
-        setBorder(BorderFactory.createCompoundBorder(
-            BorderFactory.createEmptyBorder(1, 1, 0, 1),
+        // Setup LayerMenuItem configurations (layout, border and background)
+        this.setLayout(new BorderLayout());
+        this.setBorder(BorderFactory.createCompoundBorder(
+            BorderFactory.createEmptyBorder(1, 1, 0, 1), // Spacing between each component
             BorderFactory.createLineBorder(new Color(197, 197, 197), 1, true)
         ));
-        setBackground(new Color(227,227,227));
+        this.setBackground(VISIBLE_BACKGROUND_COLOUR);
 
-        this.layerLabel = new JLabel(layerName);
+        // Layer's text/name
+        layerLabel.setText(layerName);
+        layerLabel.setForeground(VISIBLE_ICON_COLOUR);
 
-        this.displayButton.setBorder(new LineBorder(new Color(0,0,0,0), 10, true));
-        this.removeButton.setBorder(new LineBorder(new Color(0,0,0,0), 10, true));    
+        // Remove borders from the icon buttons
+        displayButton.setBorder(new LineBorder(new Color(0,0,0,0), 10, true));
+        removeButton.setBorder(new LineBorder(new Color(0,0,0,0), 10, true));    
 
-        add(this.displayButton, BorderLayout.WEST);
-        add(this.layerLabel, BorderLayout.CENTER);
-        add(this.removeButton, BorderLayout.EAST);
+        // Add all components to the LayerMenuItem (JPanel)
+        this.add(displayButton, BorderLayout.WEST);
+        this.add(layerLabel, BorderLayout.CENTER);
+        this.add(removeButton, BorderLayout.EAST);
 
-        this.displayButton.addActionListener(new ActionListener() { 
+        // Add actions to switch visibility (using the eye open/shut icons)
+        displayButton.addActionListener(new ActionListener() { 
             @Override
             public void actionPerformed(ActionEvent e) { changeDisplayState(); }
         });
 
-        this.removeButton.addActionListener(new ActionListener() { 
+        // Add actions to remove a layer (using the trash icon)
+        removeButton.addActionListener(new ActionListener() { 
             @Override
             public void actionPerformed(ActionEvent e) { removeLayer(Manager); }
         });
 
     }
 
-    public void changeDisplayState() {
-        boolean isVisible = this.displayButton.getIcon().equals(this.visibleIcon);
-        this.displayButton.setIcon(isVisible ? this.invisibleIcon : this.visibleIcon); 
-    }
+    // TODO: ADD FUNCTIONALITY FOR ACTIVE COLOURS
+    // Change the look of each layer in the menu to indicate it's current status
+    public void changeDisplayState() {  
+        // Note: Not an if-else since the "Active" state hasn't yet been implemented
+        if(isVisible) {
+            this.setBackground(HIDDEN_BACKGROUND_COLOUR);
+            displayButton.setIcon(HIDDEN_EYE_SHUT_ICON);
+            layerLabel.setForeground(HIDDEN_ICON_COLOUR);
+            removeButton.setIcon(HIDDEN_TRASH_ICON);
+        } 
+        if(!isVisible) {
+            this.setBackground(VISIBLE_BACKGROUND_COLOUR);
+            displayButton.setIcon(VISIBLE_EYE_OPEN_ICON);
+            layerLabel.setForeground(VISIBLE_ICON_COLOUR);
+            removeButton.setIcon(VISIBLE_TRASH_ICON);
+        }
+        isVisible = !isVisible;
+        revalidate();
+        repaint();
+    }    
 
+    // ? Could limit this pop-up on the condition of a layer not being empty (fully transparent) for usability.
+    // Remove a layer with an aditional pop-up to limit human error
     public void removeLayer(PLayerSelector Manager) {
-        Manager.removeLayerMenuItem(this);
+        int option = JOptionPane.showOptionDialog(
+            null, "Are you sure you want to delete this layer?", "Layer Deletion",
+            JOptionPane.YES_NO_OPTION,
+            JOptionPane.QUESTION_MESSAGE,
+            createImageIcon(40, 40, "/Icons/question_mark_icon.png"),
+            null, null
+        );
+        if (option == JOptionPane.YES_OPTION) Manager.removeLayerMenuItem(this);
     }
 
 }
--- pixelPaint/app/src/main/java/org/scc200g15/gui/layerselector/PLayerSelector.java
+++ pixelPaint/app/src/main/java/org/scc200g15/gui/layerselector/PLayerSelector.java
@@ -5,13 +5,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.LineBorder;
 
@@ -19,73 +19,89 @@ import javax.swing.border.LineBorder;
  * The right hand side bar which will contain the layers
  */
 public class PLayerSelector extends JPanel {
+  private JPanel contentPanel = new JPanel();
   private JPanel titleDisplay = new JPanel();
-  private JLabel sideLabel = new JLabel("Layer Menu");
+  private JScrollPane scroll;
 
   private ArrayList<LayerMenuItem> layers = new ArrayList<LayerMenuItem>(16);
 
   private int totalCreatedLayerCount = 1;
 
   /**
-    Placeholder. 
+    SideBar which holds all the Layer Selectors
   */
   public PLayerSelector(JFrame window) {
     // Setting up parameters
-    setBorder(new BevelBorder(BevelBorder.LOWERED));
-    setPreferredSize(new Dimension(250, window.getHeight()));
-    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+    this.setBorder(new BevelBorder(BevelBorder.LOWERED));
+    this.setPreferredSize(new Dimension(250, window.getHeight()));
+    this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+    
+    // JPanel which holds all the content
+    contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
 
     // Adding the title display
-    this.titleDisplay.add(this.sideLabel);
-    add(this.titleDisplay);
+    titleDisplay.add(new JLabel("Layer Menu"));
+    contentPanel.add(titleDisplay);
 
     // Add first layers
     LayerMenuItem firstLayer = new LayerMenuItem("Layer #1", this);
     layers.add(firstLayer);
-    add(firstLayer);
+    contentPanel.add(firstLayer);
     
     // Add layer adder button
     JPanel plusLayerHolderPanel = new JPanel();
     plusLayerHolderPanel.add(createPlusLayerButton(this));
-    add(plusLayerHolderPanel);
+    contentPanel.add(plusLayerHolderPanel);
+
+    // Add the ability to scroll
+    scroll = new JScrollPane(contentPanel);
+    this.add(scroll);
 
     // Fix spacing
-    this.titleDisplay.setMaximumSize(minimizeSpacing(this.titleDisplay));
-    firstLayer.setMaximumSize(minimizeSpacing(firstLayer));
-
+    this.titleDisplay.setMaximumSize(getMaxSize(this.titleDisplay));
+    firstLayer.setMaximumSize(getMaxSize(firstLayer));
   }
 
+
   public JButton createPlusLayerButton(PLayerSelector Manager) {
+    // Add new button to create new layer (symbol: +)
     JButton addLayer = new JButton(new ImageIcon(
       new ImageIcon(getClass().getResource("/Icons/plus_icon.png"))
         .getImage()
-        .getScaledInstance(25, 25, java.awt.Image.SCALE_SMOOTH)
+        .getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH)
     ));
     addLayer.setBorder(new LineBorder(new Color(0,0,0,0), 10, true));
 
+    // Monitor addLayer button press
     addLayer.addActionListener(new ActionListener() { 
         @Override
-        public void actionPerformed(ActionEvent e) { 
+        public void actionPerformed(ActionEvent e) {
+          // Creates a new layer
           LayerMenuItem newLayer = new LayerMenuItem("Layer #" + (++totalCreatedLayerCount), Manager);
-          layers.add(newLayer);
-          add(newLayer, getComponentCount() - 1);
-          newLayer.setMaximumSize(minimizeSpacing(newLayer));
-          revalidate();
-          repaint();
+          Manager.layers.add(newLayer);
+          contentPanel.add(newLayer, contentPanel.getComponentCount() - 1);
+          newLayer.setMaximumSize(getMaxSize(newLayer));
+          refreshUI();
         }
     });
 
     return addLayer;
   }
 
-
-  public Dimension minimizeSpacing(JPanel panel) {
+  // Get Largest Dimensions of a layer item
+  public Dimension getMaxSize(JPanel panel) {
     return new Dimension(Integer.MAX_VALUE, panel.getPreferredSize().height);
   }
 
+  // Remove Layer item
   public void removeLayerMenuItem(LayerMenuItem layer) {
     layers.remove(layer);
-    remove(layer);
+    contentPanel.remove(layer);
+    refreshUI();
+  }
+
+  // Refresh the UI
+  public void refreshUI() {
     revalidate();
     repaint();
   }
