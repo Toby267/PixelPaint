277abc5227c3b6cdf9d7aa3ac407aaf80cf8618b
hotherst@lancaster.ac.uk <hotherst@lancaster.ac.uk>
2025-02-20 21:26:46 +0000

    Added the triangle and star class for the draw tool

---
+++ pixelPaint/app/src/main/java/org/scc200g15/tools/shapes/Triangle.java
@@ -0,0 +1,19 @@
+package org.scc200g15.tools.shapes;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+/**
+ * used to represents a triangle for drawing on the canvas
+ * TODO: this
+ */
+public class Triangle implements Shape {
+    /**
+     * returns the pixels for a square of width width around center
+     */
+    @Override
+    public ArrayList<Point2D> returnPixels(Point2D center, int width) {
+        ArrayList<Point2D> points = new ArrayList<Point2D>();
+        return points;
+    }
+}
