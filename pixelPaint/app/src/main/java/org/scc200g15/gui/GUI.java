3ce6bc30b1e18b711adb6b4f4d8ed9a1de6f8fb5
Isaac Harrison <i.harrison3@lancaster.ac.uk>
2025-03-10 13:16:55 +0000

    Created basic fill tool

--- pixelPaint/app/src/main/java/org/scc200g15/gui/GUI.java
+++ pixelPaint/app/src/main/java/org/scc200g15/gui/GUI.java
@@ -19,6 +19,7 @@ import org.scc200g15.gui.toolbar.PToolBar;
 import org.scc200g15.image.Image;
 import org.scc200g15.tools.DrawTool;
 import org.scc200g15.tools.EraserTool;
+import org.scc200g15.tools.FillTool;
 import org.scc200g15.tools.PanZoomTool;
 import org.scc200g15.tools.Tool;
 import org.scc200g15.tools.ToolIcons;
@@ -68,6 +69,9 @@ public class GUI extends JFrame {
 
     EraserTool eraserTool = new EraserTool();
     registerTool(eraserTool, IconManager.ERASE_ICON, "erase", "Erase Tool");
+
+    FillTool fillTool = new FillTool();
+    registerTool(fillTool, IconManager.FILL_ICON, "fill", "Fill Tool");
   }
 
   private GUI() {
