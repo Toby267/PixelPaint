76cf6623037aa8eeccd58262ae7c0cf257905dbc
NoahHamilton1 <56700333+NoahHamilton1@users.noreply.github.com>
2025-03-15 22:16:47 +0000

    Added hard coded functionality to pass in a color and have it be set onto the colorwheel. Added a styled slider for oppacity

--- pixelPaint/app/src/main/java/org/scc200g15/gui/sidebar/ColourPicker.java
+++ pixelPaint/app/src/main/java/org/scc200g15/gui/sidebar/ColourPicker.java
@@ -20,6 +20,10 @@ public class ColourPicker extends JComponent {
     // Saturation + Brigthness components
     private final int RADIUS_SB = 50;
 
+    // Colour wheel center points
+    private int x;
+    private int y;
+    
     // Current selected colour
     private Color currentColor;
 
@@ -37,6 +41,11 @@ public class ColourPicker extends JComponent {
 
     // Stores Saturation + Brigthness to search by pixel
     private BufferedImage IMAGE_SB;
+
+    private RenderingHints rh = new RenderingHints(
+        RenderingHints.KEY_ANTIALIASING,
+        RenderingHints.VALUE_ANTIALIAS_ON
+    );
 
     private final ColourPickerTools Tools = new ColourPickerTools();
 
@@ -66,54 +75,117 @@ public class ColourPicker extends JComponent {
         });    
     }
 
+
+    public void setColor(Color c) {
+        setH(c);
+        paintImmediately(0, 0, getWidth(), getHeight());
+        setSB(c);  
+        paintImmediately(0, 0, getWidth(), getHeight());
+    }
+
+    public void setH(Color c) {
+        int red = c.getRed();
+        int green = c.getGreen();
+        int blue = c.getBlue();
+
+        int max = Math.max(red, Math.max(green, blue));
+        int min = Math.min(red, Math.min(green, blue));
+
+        double hue = calculateHue(red, green, blue, max, min);
+        int radius = (RADIUS_OUTER_H + RADIUS_INNER_H) / 2;
+
+        this.mouseX = this.x + (int) Math.round(radius * Math.cos(Tools.getθ(hue))); 
+        this.mouseY = this.y + (int) Math.round(radius * Math.sin(Tools.getθ(hue)));
+
+        repaint();
+    }
+
+    public void setSB(Color c) {
+        Color tempC = null;
+        double minDifference = colorDifference(c, getPixelColor(0, 0));
+        for(int x_i = 0; x_i < this.IMAGE_SB.getWidth(); x_i++) {
+            for (int y_i = 0; y_i < this.IMAGE_SB.getHeight(); y_i++) {
+                double difference = colorDifference(c, getPixelColor(x_i, y_i));
+                if(difference < minDifference) {
+                    tempC = getPixelColor(x_i, y_i);
+                    minDifference = difference;
+                    this.mouseX = x_i;
+                    this.mouseY = y_i;
+                }
+            }
+        }
+        System.out.println("Calculated: (" + tempC.getRed() + ", " + tempC.getGreen() + ", " + tempC.getBlue() + ")");
+        repaint();
+    }
+
+
+
+    public double colorDifference(Color target, Color attempt) {
+        int r = target.getRed() - attempt.getRed();
+        int g = target.getGreen() - attempt.getGreen();
+        int b = target.getBlue() - attempt.getBlue();
+        return Math.pow(r, 2) + Math.pow(g, 2) + Math.pow(b, 2);
+    }
+
+    public double calculateHue(int red, int green, int blue, int max, int min) {
+        // Taken from: https://cs.stackexchange.com/questions/64549/convert-hsv-to-rgb-colors
+        double hue = 60;
+        if (max != min) {
+            if (max == red) hue *= (green - blue) / (double) (max - min);
+            else if (max == green) hue *= 2 + (blue - red) / (double) (max - min);
+            else hue *= 4 + (red - green) / (double) (max - min);
+            if (hue < 0) hue += 360;
+        }
+        hue /= 360.0;
+        return hue % 1.0;
+    }
+
     // * ---------------------------------- [ PAINT ] ---------------------------------- * //
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-
-        RenderingHints rh = new RenderingHints(
-            RenderingHints.KEY_ANTIALIASING,
-            RenderingHints.VALUE_ANTIALIAS_ON
-        );
-
+        
         g2.setRenderingHints(rh);
 
         // Colour wheel center points
-        int x = getWidth() / 2;
-        int y = (int) Math.round(RADIUS_OUTER_H * 1.25);
+        x = getWidth() / 2;
+        y = (int) Math.round(RADIUS_OUTER_H * 1.25);
 
         // Draw the hue color ring
         paintPickerH(g2, x, y);
 
         // Draw the hue hover tool
-        if(clickedH(this.mouseX, this.mouseY, x, y)) {
-            this.currentColor = paintHoverH(g2, this.mouseX, this.mouseY, x, y);
+        if(clickedH(this.mouseX, this.mouseY)) {
+            this.currentColor = paintHoverH(g2, this.mouseX, this.mouseY);
             this.pastX_H = this.mouseX;
             this.pastY_H = this.mouseY;
         } else 
-            this.currentColor = paintHoverH(g2, this.pastX_H, this.pastY_H, x, y);
+            this.currentColor = paintHoverH(g2, this.pastX_H, this.pastY_H);
 
-        // Setup the bufferImage to use getPixel
+        // Draw the saturation + brightness color ring
         this.IMAGE_SB = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d_temp = this.IMAGE_SB.createGraphics();
         g2d_temp.setRenderingHints(rh);
         g2d_temp.setColor(getBackground());
 
-        // Draw the saturation + brightness color ring
         paintPickerSB(g2d_temp, x, y, this.currentColor);
+
         g2d_temp.dispose();
         g2.drawImage(this.IMAGE_SB, 0, 0, null);
 
         // Draw the saturation + brightness hover tool
-        if(clickedBS(this.mouseX, this.mouseY, x, y)) {
+        if(clickedBS()) {
             this.currentColor = paintHoverSB(g2, this.mouseX, this.mouseY);
             this.pastX_BS = this.mouseX;
             this.pastY_BS = this.mouseY;
         } else 
             this.currentColor = paintHoverSB(g2, this.pastX_BS, this.pastY_BS);
+
     }
+
+
 
     // * ---------------------------------- [ HUE ] ---------------------------------- * //
 
@@ -134,7 +206,7 @@ public class ColourPicker extends JComponent {
         }
     }
 
-    public Color paintHoverH(Graphics2D g2, int x, int y, int centerX, int centerY) {
+    public Color paintHoverH(Graphics2D g2, int mouseX, int mouseY) {
         g2.setStroke(new BasicStroke(4));
 
         // Calculate radius of the circle and width of the ring
@@ -142,12 +214,12 @@ public class ColourPicker extends JComponent {
         int radius = (RADIUS_OUTER_H + RADIUS_INNER_H) / 2;
 
         // Find angle (θ) to use in calcuating hue (normalise 0-1)
-        double θ = Tools.getθ(y - centerY, x - centerX); 
+        double θ = Tools.getθ(mouseY - y, mouseX - x); 
         double hue = Tools.getHue(θ);
 
         // Find correct coordinates using trigonometry / unit circle
-        int destinationX = centerX + Tools.newXint(radius, θ); // x = r * cos(θ) 
-        int destinationY = centerX + Tools.newYint(radius, θ); // y = r * sin(θ) 
+        int destinationX = y + Tools.newXint(radius, θ); // x = r * cos(θ) 
+        int destinationY = y + Tools.newYint(radius, θ); // y = r * sin(θ) 
 
         // Create hover bubble
         Color fillColor = Color.getHSBColor((float) hue, 1.0f, 1.0f);
@@ -160,8 +232,8 @@ public class ColourPicker extends JComponent {
     }
 
 
-    private boolean clickedH(int x, int y, int centerX, int centerY) {
-        double distance = Tools.getDistance(x - centerX, y - centerY);
+    private boolean clickedH(int mouseX, int mouseY) {
+        double distance = Tools.getDistance(mouseX - x, mouseY - y);
         int tolerance = 9;
         if (this.mouseX == -1 && this.mouseY == -1) 
             return false;
@@ -177,6 +249,9 @@ public class ColourPicker extends JComponent {
         
         int θ_black = 75;
         int θ_white = 200;
+
+        // ! TODO: MAYBE LIMIT THE FILL COLOR OR MAKE IT A GRADIENT AS WELL
+        // if doesn't work add grey gradient
 
         // Add gradients from outside, which fades as distance increases
         addGradient(g2, x, y, Math.toRadians(θ_black), new Color(0, 0, 0), false);
@@ -196,11 +271,7 @@ public class ColourPicker extends JComponent {
         }
             
         int radius = (int) Math.round(RADIUS_SB / 5f);
-        int color = this.IMAGE_SB.getRGB(x, y);
-
-        // Create hover bubble
-        // From: https://stackoverflow.com/questions/25761438/understanding-bufferedimage-getrgb-output-values
-        Color fillColor = new Color((color & 0xff0000) >> 16, (color & 0xff00) >> 8, color & 0xff);
+        Color fillColor = getPixelColor(x, y);
         g2.setColor(fillColor);
         g2.fillOval(x - radius, y - radius, radius * 2, radius * 2);
         g2.setColor(Color.WHITE);
@@ -209,8 +280,8 @@ public class ColourPicker extends JComponent {
         return fillColor;
     }
 
-    private boolean clickedBS(int x, int y, int centerX, int centerY) {
-        double distance = Tools.getDistance(x - centerX, y - centerY);
+    private boolean clickedBS() {
+        double distance = Tools.getDistance(mouseX - x, mouseY - y);
         int tolerance = -1;
         if (this.mouseX == -1 && this.mouseY == -1) 
             return false;
@@ -244,7 +315,13 @@ public class ColourPicker extends JComponent {
         g2.fillOval(x - RADIUS_SB, y - RADIUS_SB, RADIUS_SB * 2, RADIUS_SB * 2);
     }
 
-    // * ------------------------- [ SATURATION / BRIGHTNESS ] ------------------------- * //
+    // From: https://stackoverflow.com/questions/25761438/understanding-bufferedimage-getrgb-output-values
+    private Color getPixelColor(int x, int y) {
+        int color = this.IMAGE_SB.getRGB(x, y);
+        return new Color((color & 0xff0000) >> 16, (color & 0xff00) >> 8, color & 0xff);
+    }
+
+    // * ------------------------- [ ACCESSOR / MUTATORS ] ------------------------- * //
 
     public Color getActiveColor() {
         return this.currentColor;
