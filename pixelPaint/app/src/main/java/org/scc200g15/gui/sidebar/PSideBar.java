d81b0b8f1ce679088e3378bb8a440be9f3b61aed
NoahHamilton1 <56700333+NoahHamilton1@users.noreply.github.com>
2025-03-16 12:42:13 +0000

    Finished opacity slider and linked it up to getActiveColor()

--- pixelPaint/app/src/main/java/org/scc200g15/gui/sidebar/PSideBar.java
+++ pixelPaint/app/src/main/java/org/scc200g15/gui/sidebar/PSideBar.java
@@ -4,14 +4,17 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 import javax.swing.border.BevelBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import org.scc200g15.gui.GUI;
 
@@ -21,48 +24,111 @@ import org.scc200g15.gui.GUI;
 public class PSideBar extends JPanel {
   ColourPicker colourPicker = new ColourPicker();
 
+  JLabel opacityValue = new JLabel("100%");
+  int opacityPercent = 100;
+
   public PSideBar(GUI window) {
     setBorder(new BevelBorder(BevelBorder.LOWERED));
 
     setPreferredSize(new Dimension(200, window.getHeight()));
     setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-    JLabel sideLabel = new JLabel("Colour Selector");
-    sideLabel.setHorizontalAlignment(SwingConstants.CENTER);
+    sliderStyling();
 
-    add(sideLabel);
+    // Title
+    JPanel title = new JPanel();
+    panelSetup(title);
+    title.add(titleLabel());
+    add(title);
 
+    // Colour Picker
+    this.colourPicker.setAlignmentY(TOP_ALIGNMENT);
     add(this.colourPicker);
+    add(Box.createVerticalStrut(8)); 
+    
+    // Opacity Slider
+    JPanel opacitySlider = new JPanel();
+    panelSetup(opacitySlider);
+    opacitySlider.add(opacitySlider());
+    opacitySlider.add(Box.createHorizontalStrut(2));
+    opacitySlider.add(opacityValueLabel());
+    opacitySlider.add(Box.createHorizontalStrut(5));
+    add(opacitySlider);
+    add(Box.createVerticalStrut(8));
 
+    // Button
+    JPanel button = new JPanel();
+    panelSetup(button);
+    button.add(button());
+    add(button);
 
+    // Bottom Spacing
+    add(Box.createVerticalStrut(370));
+  }
+
+  public void panelSetup(JPanel panel) {
+    panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+    panel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 40));
+  }
+
+  public JLabel titleLabel() {
+    JLabel label = new JLabel("Colour Selector");
+    label.setPreferredSize(new Dimension(200, 20));
+    label.setHorizontalAlignment(JLabel.CENTER);
+    return label;
+  }
+
+  public JLabel opacityValueLabel() {
+    opacityValue.setPreferredSize(new Dimension(35, 20));
+    opacityValue.setHorizontalAlignment(SwingConstants.CENTER);
+    return opacityValue;
+  }
+
+  public JSlider opacitySlider() {
     JSlider slider = new JSlider(0, 100, 100);
+    slider.setPreferredSize(new Dimension(120, 20));
 
+    slider.addChangeListener(new ChangeListener() {
+      @Override
+      public void stateChanged(ChangeEvent e) {
+        opacityPercent = ((JSlider) e.getSource()).getValue();
+        opacityValue.setText(opacityPercent + "%");
+      }
+    });
 
+    return slider;
+  }
 
-    add(slider);
-
+  public JButton button() {
     JButton button = new JButton("PRESS");
+
     button.addActionListener((ActionEvent e) -> {
       Color c = new Color(248, 122, 250); // ! Manually add the changes, moves it into input text
       this.colourPicker.setColor(c);
 
-      Color out = this.colourPicker.getActiveColor();
-      System.out.println("Active: (" + out.getRed() + ", " + out.getGreen() + ", " + out.getBlue() + ")");
-
-
-          
+      Color out = getActiveColor();
+      System.out.println("Active: (" + out.getRed() + ", " + out.getGreen() + ", " + out.getBlue() + ", " + out.getAlpha() + ")");
     });
 
-
-    add(button);
+    return button;
   }
 
 
-
+  public void sliderStyling() {
+    UIManager.put("Slider.thumbSize", new Dimension(12, 12));
+    UIManager.put("Slider.trackWidth", 4);
+  }
 
 
   public Color getActiveColor() {
-    return this.colourPicker.getActiveColor();
+    Color c = this.colourPicker.getActiveColor();
+    return new Color(
+      c.getRed()     / (float) 255.0, 
+      c.getGreen()   / (float) 255.0, 
+      c.getBlue()    / (float) 255.0, 
+      opacityPercent / (float) 100.0
+    );
   }
+
 
 }
