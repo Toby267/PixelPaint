56ecd073998e649063db0616d7e1c59578758621
Isaac Harrison <i.harrison3@lancaster.ac.uk>
2025-04-22 20:32:24 +0100

    Removed Debug Prints From Color Dropper

--- pixelPaint/app/src/main/java/org/scc200g15/tools/ColourDropperTool.java
+++ pixelPaint/app/src/main/java/org/scc200g15/tools/ColourDropperTool.java
@@ -15,21 +15,14 @@ public class ColourDropperTool implements Tool {
 
     @Override
     public void mouseReleased(PCanvas c, java.awt.event.MouseEvent e) {
-        System.out.println("REACHED");
         Point2D point = c.getPixelPoint(e.getPoint());
         
         int x = (int) Math.round(point.getX());
         int y = (int) Math.round(point.getY());
-        System.out.println("(" + x + ", " + y + ")");
-
-
 
         // From: https://stackoverflow.com/questions/25761438/understanding-bufferedimage-getrgb-output-values
         int color = GUI.getInstance().getCanvas().getBufferedImage().getRGB(x, y);
         Color colour = new Color((color & 0xff0000) >> 16, (color & 0xff00) >> 8, color & 0xff);
-        System.out.println("RED = ");
-        System.out.println("BLUE = ");
-        System.out.println("GREEN = ");
         GUI.getInstance().getSideBar().setColourWheel(colour); // SET ACTIVE COLOUR IN COLOR WHEEL
     }
 
